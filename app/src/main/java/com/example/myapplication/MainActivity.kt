849b30813e41ef2e0b6d5fe9948d43c368a54924
sztoslol <kubuszeski05@wp.pl>
2022-03-24 12:24:12 +0100

    jest git

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,13 +2,53 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var kobieta = findViewById<RadioButton>(R.id.RadioKobieta)
+        var mezczyzna = findViewById<RadioButton>(R.id.RadioMezczyzna)
+        var wybranaPlec = ""
+        if (kobieta.isChecked == true)
+        {
+            wybranaPlec = "Kobieta"
+        }
+        else
+        {
+            wybranaPlec = "Mężczyzna"
+        }
+
+        var sport = findViewById<CheckBox>(R.id.checkBoxSport)
+        var muzyka = findViewById<CheckBox>(R.id.checkBoxMuzyka)
+        var turystyka = findViewById<CheckBox>(R.id.checkBoxTurystyka)
+        var gry = findViewById<CheckBox>(R.id.checkBoxGry)
+        var wybraneZainteresowania = "Zainteresowania: "
+
+        if (sport.isChecked == true)
+        {
+            wybraneZainteresowania.plus("Sport ")
+        }
+        if (muzyka.isChecked == true)
+        {
+            wybraneZainteresowania.plus("Muzyka ")
+        }
+        if (turystyka.isChecked == true)
+        {
+            wybraneZainteresowania.plus("Turystyka ")
+        }
+        if (gry.isChecked == true)
+        {
+            wybraneZainteresowania.plus("Gry ")
+        }
+
+        var wiek = findViewById<>()
+
+        findViewById<Button>(R.id.ButtonZakoncz).setOnClickListener {
+            findViewById<TextView>(R.id.Wynik).text =
+         }
     }
-
-
 
 }
